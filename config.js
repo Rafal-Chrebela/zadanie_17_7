47439480a7c9e4fdc613331d9b9e790ff9db5d48
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-04-19 22:58:20 +0200

    Add files with content

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GOOGLE_CLIENT_ID:
+    "674222821589-edna4so4r5jb4l8oorf7edjbe1atiak4.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "p0AMZLVH5zCIIDRE4r_qix2c",
+  CALLBACK_URL: "http://localhost:3000/auth/google/callback"
+};
